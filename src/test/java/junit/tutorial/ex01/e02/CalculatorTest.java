4b1e1e87cdfd036522dfbd86ad6c1dd7dadb68f1
natsumifukuhara <natsumi.fukuhara@rakus-partners.co.jp>
2021-05-20 11:56:17 +0900

    third commit

---
+++ src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
@@ -0,0 +1,37 @@
+package junit.tutorial.ex01.e02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	
+
+	@Test
+	void test() {
+		Calculator calculator = new Calculator();
+		assertEquals("divide by zero.",calculator.divide(1, 0));
+	}
+
+}
