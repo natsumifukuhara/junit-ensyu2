d76b23c7b697a529a8aa36c2a0b8d00620302004
natsumifukuhara <natsumi.fukuhara@rakus-partners.co.jp>
2021-05-20 13:32:53 +0900

    fifth commit

---
+++ src/test/java/junit/tutorial/ex01/e04/NumberUtilsTset.java
@@ -0,0 +1,45 @@
+package junit.tutorial.ex01.e04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NumberUtilsTset {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	@Test
+	void test() {
+		NumberUtils numberUtils = new NumberUtils();
+		assertTrue(numberUtils.isEven(10));
+	}
+	
+	@Test
+	void test2() {
+		NumberUtils numberUtils = new NumberUtils();
+		assertFalse(numberUtils.isEven(7));
+			
+
+}
+}
+
